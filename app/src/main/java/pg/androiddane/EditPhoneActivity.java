1df43c5c2a5201264fe825f0e62201b1c52baecd
Patryk Glegoła <patryk.glegola@gmail.com>
2017-04-21 12:44:11 +0200

    added EditPhoneAcitvity

---
+++ app/src/main/java/pg/androiddane/EditPhoneActivity.java
@@ -0,0 +1,13 @@
+package pg.androiddane;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditPhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_phone);
+    }
+}
