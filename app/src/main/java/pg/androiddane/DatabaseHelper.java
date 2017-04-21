6bd3fff7e69825b2418869b2acf29998f3fa8dd7
Patryk Glegoła <patryk.glegola@gmail.com>
2017-04-21 10:59:50 +0200

    DatabaseHelper update

--- app/src/main/java/pg/androiddane/DatabaseHelper.java
+++ app/src/main/java/pg/androiddane/DatabaseHelper.java
@@ -4,21 +4,22 @@ package pg.androiddane;
         import android.database.sqlite.SQLiteDatabase;
         import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by Patryk on 2017-04-13.
- */
 
 public class DatabaseHelper extends SQLiteOpenHelper{
     public final static int DB_VERSION = 1;
     public final static String ID = "_id";
-    public final static String DB_NAME = "db_name";
-    public final static String TABLE_NAME = "table_name";
-    public final static String COL1 = "column_1_name";
-    public final static String COL2 = "column_2_name";
+    public final static String DB_NAME = "PhonesDB";
+    public final static String TABLE_NAME = "Phones";
+    public final static String COL_PRODUCENT = "producent";
+    public final static String COL_MODEL = "model";
+    public final static String COL_ANDROID_VERSION = "android version";
+    public final static String COL_WWW = "www";
     public final static String DB_create = "CREATE TABLE " + TABLE_NAME +
             "("+ID+" integer primary key autoincrement, " +
-            COL1 + " text not null, " +
-            COL2 + " text);";
+            COL_PRODUCENT + " text not null, " +
+            COL_MODEL + " text not null, " +
+            COL_ANDROID_VERSION + " text not null, " +
+            COL_WWW + " text);";
     private static final String DB_delete = "DROP TABLE IF EXIST "+TABLE_NAME;
 
     public DatabaseHelper(Context context) {
