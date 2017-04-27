33b02129fa0c44365ecfa58aff2f3d927e8dc932
Patryk Glegoła <patryk.glegola@gmail.com>
2017-04-27 22:25:00 +0200

    PhonesProvider added and configured

--- app/src/main/java/pg/androiddane/DatabaseHelper.java
+++ app/src/main/java/pg/androiddane/DatabaseHelper.java
@@ -27,6 +27,8 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         super(context,DB_NAME,null,DB_VERSION);
     }
 
+
+
     @Override
     public void onCreate(SQLiteDatabase db)
     {
---
+++ app/src/main/java/pg/androiddane/PhonesProvider.java
@@ -0,0 +1,133 @@
+package pg.androiddane;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+
+public class PhonesProvider extends ContentProvider {
+
+    //pole przechowujące referencję do obiektu pomocnika bazy:
+    private DatabaseHelper mDatabaseHelper;
+    //identyfikator dostawcy - pozwala na odróżnienie naszego dostawcy od innych:
+    private static final String AUTHORITY = "pg.androiddane.myProvider";
+    //stała - aby nie trzeba było wpisywać tekstu samodzielnie:
+    public static final Uri URI_CONTENTS = Uri.parse("content://" + AUTHORITY
+                                        + "/" + DatabaseHelper.TABLE_NAME);
+    //stałe pozwalające zidentyfikować rodzaj rozpoznanego URI:
+    private static final int WHOLE_TABLE = 1;
+    private static final int SELECTED_ROW = 2;
+    //UriMatcher z pustym korzeniem drzewa URI (NO_MATCH):
+    private static final UriMatcher uriMatch = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        //dodanie rozpoznawanych URI:
+        uriMatch.addURI(AUTHORITY, DatabaseHelper.TABLE_NAME, WHOLE_TABLE);
+        uriMatch.addURI(AUTHORITY, DatabaseHelper.TABLE_NAME + "/#", SELECTED_ROW);
+    }
+
+    @Override
+    public boolean onCreate() {
+        mDatabaseHelper = new DatabaseHelper(getContext());
+        return false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection,
+                        @Nullable String selection, @Nullable String[] selectionArgs,
+                        @Nullable String sortOrder) {
+        //sprawdzenie typu Uri i zapisanie wyniku
+        int typeOfUri = uriMatch.match(uri);
+
+        //otwieranie magazynu - bazy danych telefonów:
+        SQLiteDatabase phonesDB = mDatabaseHelper.getWritableDatabase();
+
+        Cursor phonesCursor = null;
+        switch (typeOfUri) {
+            //gdy oczytujemy dane z całej tabeli, przekazujemy parametry z wywołania metody query()
+            // dostawcy do metody query() bazy
+            case WHOLE_TABLE:
+                phonesCursor = phonesDB.query(false, DatabaseHelper.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder, null, null);
+                break;
+            //gdy odczytujemy dane jednego wiersza, do argumentu selection dodajemy wartość klucza,
+            // który określa żądany wiersz. Wykorzystujemy do tego metodę pomocniczą
+            // addIdToSelection()
+            case SELECTED_ROW:
+                phonesCursor = phonesDB.query(false, DatabaseHelper.TABLE_NAME, projection,
+                        addIdToSelection(selection, uri),
+                        selectionArgs, null, null, sortOrder, null, null);
+                break;
+            //jeśli URI nie zostanie rozpoznane, zgłaszany jest wyjątek
+            default:
+                throwUnknownUriException(uri);
+        }
+        //monitorowanie zmiany danych URI:
+        phonesCursor.setNotificationUri(
+                getContext().getContentResolver(), uri);
+
+        return phonesCursor;
+    }
+
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values,
+                      @Nullable String selection, @Nullable String[] selectionArgs) {
+        int typeOfUri = uriMatch.match(uri);
+        SQLiteDatabase phonesDB = mDatabaseHelper.getWritableDatabase();
+
+        int amountOfUpdated = 0;
+        switch (typeOfUri) {
+            case WHOLE_TABLE:
+                amountOfUpdated = phonesDB.update(DatabaseHelper.TABLE_NAME,
+                        values, selection, selectionArgs);
+                break;
+            case SELECTED_ROW:
+                amountOfUpdated = phonesDB.update(DatabaseHelper.TABLE_NAME,
+                        values, addIdToSelection(selection, uri), selectionArgs);
+                break;
+            default:
+                throwUnknownUriException(uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return amountOfUpdated;
+    }
+
+    private void throwUnknownUriException(@NonNull Uri uri) {
+        throw new IllegalArgumentException("Unknown URI: " + uri);
+    }
+
+
+    private String addIdToSelection(String selection, Uri uri) {
+        if (selection != null && !selection.equals(""))
+            selection = selection + " and " + DatabaseHelper.ID + "=" + uri.getLastPathSegment();
+        else
+            selection = DatabaseHelper.ID + "=" + uri.getLastPathSegment();
+        return selection;
+    }
+}
