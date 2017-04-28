932256ba7d165c4032a0c7aead52af5b46d845ce
Patryk Glegoła <patryk.glegola@gmail.com>
2017-04-28 12:35:02 +0200

    TODO: adding and editing phones

--- app/src/main/java/pg/androiddane/EditPhoneActivity.java
+++ app/src/main/java/pg/androiddane/EditPhoneActivity.java
@@ -1,13 +1,72 @@
 package pg.androiddane;
 
+import android.content.ContentUris;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditPhoneActivity extends AppCompatActivity {
+
+    private long rowId;
+    private EditText editProducentET;
+    private EditText editModelET;
+    private EditText editAndroidVersionET;
+    private EditText editWwwET;
+    private Button saveButton;
+    private Button cancelButton;
+    private Button wwwButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_phone);
+        findAllViews();
+        rowId = -1;
+        if(savedInstanceState != null) {
+            rowId = savedInstanceState.getLong((DatabaseHelper.ID));
+        }
+        else {
+            Bundle bundle = getIntent().getExtras();
+            if (bundle != null)
+                rowId = bundle.getLong(DatabaseHelper.ID);
+        }
+        if (rowId != -1)
+            fillFields();
+        //obsługa przycisków
+
+
+    }
+
+    private void fillFields() {
+        String projection[] = { DatabaseHelper.COL_PRODUCENT, DatabaseHelper.COL_MODEL,
+                            DatabaseHelper.COL_ANDROID_VERSION, DatabaseHelper.COL_WWW};
+        Cursor phonesCursor = getContentResolver().query(
+                ContentUris.withAppendedId(PhonesProvider.URI_CONTENTS, rowId),
+                projection, null, null, null);
+        phonesCursor.moveToFirst();
+        int columnIndex = phonesCursor.getColumnIndexOrThrow(DatabaseHelper.COL_PRODUCENT);
+        String value = phonesCursor.getString(columnIndex);
+        editProducentET.setText(value);
+        editModelET.setText(phonesCursor.getString(phonesCursor.getColumnIndexOrThrow(
+                DatabaseHelper.COL_MODEL)));
+        editAndroidVersionET.setText(phonesCursor.getString(phonesCursor.getColumnIndexOrThrow(
+                DatabaseHelper.COL_ANDROID_VERSION)));
+        editWwwET.setText(phonesCursor.getString(phonesCursor.getColumnIndexOrThrow(
+                DatabaseHelper.COL_WWW)));
+        phonesCursor.close();
+
+    }
+
+    private void findAllViews() {
+        editProducentET = (EditText) findViewById(R.id.producentEdit);
+        editModelET = (EditText) findViewById(R.id.modelEdit);
+        editAndroidVersionET = (EditText) findViewById(R.id.androidVersionEdit);
+        editWwwET = (EditText) findViewById(R.id.wwwEdit);
+        saveButton = (Button) findViewById(R.id.activity_edit_phone_save_button);
+        cancelButton = (Button) findViewById(R.id.activity_edit_phone_cancel_button);
+        wwwButton = (Button) findViewById(R.id.activity_edit_phone_www_button);
     }
 }
--- app/src/main/java/pg/androiddane/MainActivity.java
+++ app/src/main/java/pg/androiddane/MainActivity.java
@@ -2,14 +2,14 @@ package pg.androiddane;
 
 import android.app.ListActivity;
 import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends ListActivity implements android.support.v4.app.LoaderManager.LoaderCallbacks<Cursor> {
+public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private SimpleCursorAdapter cursorAdapter;
 
@@ -22,18 +22,19 @@ public class MainActivity extends ListActivity implements android.support.v4.app
 
     private void setFields() {
         //inicjalizowanie loadera:
-        getLoaderManager().initLoader(0,null,null);
+        getLoaderManager().initLoader(0, null, this);
         //utworzenie mapowania między kolumnami tabeli a kolumnami wyświetlanej listy:
-        String[] mapFrom = new String[] { DatabaseHelper.COL_PRODUCENT, DatabaseHelper.COL_MODEL};
-        int [] mapTo = new int[] {R.id.producentText, R.id.modelEdit};
+        String[] mapFrom = new String[]{DatabaseHelper.COL_PRODUCENT, DatabaseHelper.COL_MODEL};
+        int[] mapTo = new int[]{R.id.producentText, R.id.modelEdit};
         //adapter wymaga, aby w wyniku zapytania znajdowała się kolumna _id
         cursorAdapter = new SimpleCursorAdapter(this, R.layout.phone_list_row, null, mapFrom, mapTo, 0);
         setListAdapter(cursorAdapter);
     }
 
+
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        String[] projection = { DatabaseHelper.ID,
+        String[] projection = {DatabaseHelper.ID,
                 DatabaseHelper.COL_PRODUCENT,
                 DatabaseHelper.COL_MODEL};
         CursorLoader cursorLoader = new CursorLoader(this, PhonesProvider.URI_CONTENTS, projection, null, null, null);
@@ -41,13 +42,12 @@ public class MainActivity extends ListActivity implements android.support.v4.app
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+    public void onLoadFinished(android.content.Loader<Cursor> loader, Cursor data) {
         cursorAdapter.swapCursor(data);
     }
 
     @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        cursorAdapter.swapCursor(null);
-    }
-
+    public void onLoaderReset(android.content.Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null)
+;    }
 }
