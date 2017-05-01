7099f9c2763474b0ab747320477d8eb88dc62111
Patryk Glegoła <patryk.glegola@gmail.com>
2017-05-02 00:02:50 +0200

    added editind and adding phones backend book p. 127 TODO: action bar, update onCreate in EditPhoneActivity

--- app/src/main/java/pg/androiddane/EditPhoneActivity.java
+++ app/src/main/java/pg/androiddane/EditPhoneActivity.java
@@ -1,19 +1,25 @@
 package pg.androiddane;
 
 import android.content.ContentUris;
+import android.content.ContentValues;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditPhoneActivity extends AppCompatActivity {
 
+    private static final int NEW_ROW = -1;
+
     private long rowId;
-    private EditText editProducentET;
-    private EditText editModelET;
-    private EditText editAndroidVersionET;
-    private EditText editWwwET;
+    private EditText producentET;
+    private EditText modelET;
+    private EditText androidVersionET;
+    private EditText wwwET;
     private Button saveButton;
     private Button cancelButton;
     private Button wwwButton;
@@ -24,7 +30,7 @@ public class EditPhoneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_phone);
         findAllViews();
-        rowId = -1;
+        rowId = NEW_ROW;
         if(savedInstanceState != null) {
             rowId = savedInstanceState.getLong((DatabaseHelper.ID));
         }
@@ -33,38 +39,100 @@ public class EditPhoneActivity extends AppCompatActivity {
             if (bundle != null)
                 rowId = bundle.getLong(DatabaseHelper.ID);
         }
-        if (rowId != -1)
+        if (rowId != NEW_ROW)
+            //jeśli edytujemy istniejący wiersz tabei telefonów, wywołujemy metodę wypełniającą
+            // pola tekstowe wg danych z tego wiersza:
             fillFields();
-        //obsługa przycisków
 
+        //listenery przycisków:
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveButtonClick();
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelButtonClick();
+            }
+        });
+    }
 
+    //nadpisanie metody onSaveInstanceState, aby zabezpieczyć aplikację przed utratą identyfikatora
+    // id edytowanego wiersza tabeli z bazy w przypadku zresetowania aktywności:
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(DatabaseHelper.ID, rowId);
+    }
+
+    //obsługa przycisku "zapisz":
+    private void saveButtonClick() {
+        if(areTextFieldsNotEmpty()) { //jeśli pola nie są puste, zapisujemy dane do bazy:
+            ContentValues values = new ContentValues();
+            values.put(DatabaseHelper.COL_PRODUCENT, producentET.getText().toString());
+            values.put(DatabaseHelper.COL_MODEL, modelET.getText().toString());
+            values.put(DatabaseHelper.COL_ANDROID_VERSION, androidVersionET.getText().toString());
+            values.put(DatabaseHelper.COL_WWW, wwwET.getText().toString());
+            if(rowId == NEW_ROW) {
+                Uri newRowUri = getContentResolver().insert(PhonesProvider.URI_CONTENTS, values);
+                rowId = Integer.parseInt(newRowUri.getLastPathSegment());
+            }
+            else {
+                int changedRows = getContentResolver().update(ContentUris.withAppendedId(
+                        PhonesProvider.URI_CONTENTS, rowId), values, null, null);
+            }
+            setResult(RESULT_OK); //ustawienie wyniku działania aktywności jako OK
+            finish(); // zakończenie aktywności
+        }
+        else //jeśli któreś z pól jest puste, wyświetlany jest stosowny komunikat
+            Toast.makeText(this, getString(R.string.fill_fields_toast), Toast.LENGTH_SHORT).show();
+    }
+
+    //obsługa przycisku "anuluj":
+    private void cancelButtonClick() {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    private boolean areTextFieldsNotEmpty() {
+        return !(producentET.getText().toString().isEmpty()
+        || modelET.getText().toString().isEmpty()
+        || androidVersionET.getText().toString().isEmpty()
+        || wwwET.getText().toString().isEmpty() );
     }
 
     private void fillFields() {
+        //tablica określająca które kolumny chcemy pobrać z dostawcy:
         String projection[] = { DatabaseHelper.COL_PRODUCENT, DatabaseHelper.COL_MODEL,
                             DatabaseHelper.COL_ANDROID_VERSION, DatabaseHelper.COL_WWW};
+        //odpytanie dostawcy treści i zwrócenie kursora:
         Cursor phonesCursor = getContentResolver().query(
                 ContentUris.withAppendedId(PhonesProvider.URI_CONTENTS, rowId),
                 projection, null, null, null);
+        //przestawienie kursora na pierwszy element
         phonesCursor.moveToFirst();
-        int columnIndex = phonesCursor.getColumnIndexOrThrow(DatabaseHelper.COL_PRODUCENT);
-        String value = phonesCursor.getString(columnIndex);
-        editProducentET.setText(value);
-        editModelET.setText(phonesCursor.getString(phonesCursor.getColumnIndexOrThrow(
+        //odczytanie wartości od dostawcy i wpisanie do pól tekstowych:
+        producentET.setText(phonesCursor.getString(phonesCursor.getColumnIndexOrThrow(
+                DatabaseHelper.COL_PRODUCENT)));
+        modelET.setText(phonesCursor.getString(phonesCursor.getColumnIndexOrThrow(
                 DatabaseHelper.COL_MODEL)));
-        editAndroidVersionET.setText(phonesCursor.getString(phonesCursor.getColumnIndexOrThrow(
+        androidVersionET.setText(phonesCursor.getString(phonesCursor.getColumnIndexOrThrow(
                 DatabaseHelper.COL_ANDROID_VERSION)));
-        editWwwET.setText(phonesCursor.getString(phonesCursor.getColumnIndexOrThrow(
+        wwwET.setText(phonesCursor.getString(phonesCursor.getColumnIndexOrThrow(
                 DatabaseHelper.COL_WWW)));
+        //zamknięcie kursora:
         phonesCursor.close();
 
     }
 
+    //metoda pzydzielająca polom odpowiednie elementy XML
     private void findAllViews() {
-        editProducentET = (EditText) findViewById(R.id.producentEdit);
-        editModelET = (EditText) findViewById(R.id.modelEdit);
-        editAndroidVersionET = (EditText) findViewById(R.id.androidVersionEdit);
-        editWwwET = (EditText) findViewById(R.id.wwwEdit);
+        producentET = (EditText) findViewById(R.id.producentEdit);
+        modelET = (EditText) findViewById(R.id.modelEdit);
+        androidVersionET = (EditText) findViewById(R.id.androidVersionEdit);
+        wwwET = (EditText) findViewById(R.id.wwwEdit);
         saveButton = (Button) findViewById(R.id.activity_edit_phone_save_button);
         cancelButton = (Button) findViewById(R.id.activity_edit_phone_cancel_button);
         wwwButton = (Button) findViewById(R.id.activity_edit_phone_www_button);
--- app/src/main/java/pg/androiddane/MainActivity.java
+++ app/src/main/java/pg/androiddane/MainActivity.java
@@ -3,14 +3,19 @@ package pg.androiddane;
 import android.app.ListActivity;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
+    private static final int NEW_ELEMENT = -1;
     private SimpleCursorAdapter cursorAdapter;
 
     @Override
@@ -21,7 +26,7 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
     }
 
     private void setFields() {
-        //inicjalizowanie loadera:
+        //inicjalizacja loadera:
         getLoaderManager().initLoader(0, null, this);
         //utworzenie mapowania między kolumnami tabeli a kolumnami wyświetlanej listy:
         String[] mapFrom = new String[]{DatabaseHelper.COL_PRODUCENT, DatabaseHelper.COL_MODEL};
@@ -50,4 +55,39 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
     public void onLoaderReset(android.content.Loader<Cursor> loader) {
         cursorAdapter.swapCursor(null)
 ;    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        return super.onMenuItemSelected(featureId, item);
+
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        addOrEditElement(id);
+    }
+    private void createNewElement() {
+        addOrEditElement((long) NEW_ELEMENT);
+    }
+
+    private void addOrEditElement(long id) {
+        Intent intent = new Intent(this, EditPhoneActivity.class);
+        intent.putExtra(DatabaseHelper.ID, id);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //restart loadera, jako że dane mogły zostać zmodyfikowane:
+        getLoaderManager().restartLoader(0, null, this);
+    }
+
+
 }
