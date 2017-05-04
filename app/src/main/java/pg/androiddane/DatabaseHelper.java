c49c8d5d2c160cc31cab998d92c9979e25f2e1ee
Patryk Glegoła <patryk.glegola@gmail.com>
2017-05-04 18:45:41 +0200

    App done, comments added

--- app/src/main/java/pg/androiddane/DatabaseHelper.java
+++ app/src/main/java/pg/androiddane/DatabaseHelper.java
@@ -4,9 +4,14 @@ package pg.androiddane;
         import android.database.sqlite.SQLiteDatabase;
         import android.database.sqlite.SQLiteOpenHelper;
 
+/**
+ * Klasa przechowująca dane o bazie, nazwach tabel i kolumn
+ * oraz polecenia do tworzenia i usuwania tabeli
+ */
 
 public class DatabaseHelper extends SQLiteOpenHelper{
     public final static int DB_VERSION = 1;
+    //zdefiniowanie nazwy bazy, tabeli i kolumn:
     public final static String ID = "_id";
     public final static String DB_NAME = "PhonesDB";
     public final static String TABLE_NAME = "Phones";
@@ -14,12 +19,14 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public final static String COL_MODEL = "model";
     public final static String COL_ANDROID_VERSION = "android_version";
     public final static String COL_WWW = "www";
+    //zdefiniowanie polecenia tworzenia tabeli
     public final static String DB_create = "CREATE TABLE " + TABLE_NAME + "("+
             ID +" integer primary key autoincrement, " +
             COL_PRODUCER + " text not null, " +
             COL_MODEL + " text not null, " +
             COL_ANDROID_VERSION + " text not null, " +
             COL_WWW + " text);";
+    //zdefiniowanie polecenia usuwania tabeli:
     private static final String DB_delete = "DROP TABLE IF EXIST "+TABLE_NAME;
 
     public DatabaseHelper(Context context) {
